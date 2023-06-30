c716d63f43f18ca24ff9dd2baad8142c04623052
James <james@relay.club>
2023-06-30 02:22:47 +0000

    Add todo

--- todo.js
+++ todo.js
@@ -1,1 +1,76 @@
-// This file will be provided as a pull request at the time of your interview.
+const yargs = require('yargs');
+const fs = require('fs');
+
+let toDoList = [];
+
+const loadList = () => {
+    try {
+        const list = require('./to-do-list.json');
+            toDoList = list.sort();
+    } catch (err) {
+        toDoList = [];
+}
+    }
+
+function saveList(logWhenDone)
+{
+    // TODO: maybe we need try/catch
+    // try {
+        fs.writeFile('./to-do-list.json', JSON.stringify(toDoList));
+       if (logWhenDone) console.log('saved')
+    // }
+    // catch(e)
+    // console.error(e) {}
+}
+
+const argv = yargs
+    .command('add', 'Add a task to the list', {
+        task: {
+            describe: 'The task to add',
+            demand: true,
+            alias: 't'
+        }
+    })
+    .command('list', 'List all tasks')
+    .command('done', 'Remove a task', {
+        task: {
+            describe: 'The task number to remove',
+            demand: true,
+            alias: 'n'
+        }
+    })
+    .command('delete', 'Mark a task as complete', {
+        task: {
+            describe: 'The task number to complete',
+            demand: true,
+            alias: 'n'
+        }
+    })
+    .help()
+    .argv;
+
+loadList();
+
+let command = argv._[0];
+
+if (command === 'add') {
+    var task = argv.task;
+    toDoList.push(task);
+    saveList(true);
+    console.log("${task} added to the list.");
+} else if (command === 'list') {
+    console.log('To-Do List:');
+    toDoList.forEach((task, index) => console.log(`  ${index + 1}: ${task}`));
+} else if (command === 'done') {
+    var index = argv.task;
+    console.log(`"${toDoList[index - 1]}" marked as done.`);
+    toDoList[index] = toDoList[index]+=' (done)';
+    console.log(toDoList)
+    fs.writeFileSync('./to-do-list.json', JSON.stringify(toDoList))
+    loadList();
+
+}else if (command === 'delete') {
+    var index = argv.task;
+    console.log(`"${toDoList[index - 1]}" deleted`);
+    delete toDoList[index - 1];
+}
--- todo.test.js
+++ todo.test.js
@@ -1,1 +1,59 @@
-// This file will be provided as a pull request at the time of your interview.
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+describe('todo.js', () => {
+  const listFilePath = './to-do-list.json';
+
+  beforeEach(async() => {
+    await fs.promises.writeFile(listFilePath, '[]');
+  });
+
+  test('add a task to the list', () => {
+    const taskName = 'Buy Milk';
+
+    // Add task to the list
+    execSync(`node todo.js add -t "${taskName}"`);
+    const list = require(listFilePath);
+    // Check if the task is added to the list
+    expect(list).toHaveLength(1);
+    expect(list).toContain(taskName);
+  });
+
+  test('list all tasks', () => {
+    // Add some tasks to the list
+    const tasks = ['Task 1', 'Task 2', 'Task 3'];
+    tasks.forEach((taskName) => {
+      execSync(`node todo.js add -t "${taskName}"`);
+    });
+
+    // List all tasks and check if they are listed correctly
+    const output = execSync('node todo.js list').toString();
+    expect(output).toContain('Taks 1');
+    expect(output).toContain('Task 2');
+    expect(output).toContain('Task 3');
+  });
+
+  // Add some tasks to the list
+  const tasks = ['Task 1', 'Task 2', 'Task 3'];
+  tasks.forEach((taskName) => {
+    execSync(`node todo.js add -t "${taskName}"`);
+  });
+
+  test('remove a task', () => {
+    // Mark a task as done to remove from the list
+    execSync('node todo.js rm -n 2').toString();
+    const list = require(listFilePath);
+    // Check if the task is removed from the list
+    expect(list.includes('Task 2')).toBe(false);
+  });
+
+  test('mark a task as complete', () => {
+    const taskName = 'Buy Milk';
+    // Add task to the list
+    execSync(`node todo.js add -t "${taskName}"`);
+    // Mark a task as done
+    execSync('node todo.js done -n 1')
+    const listAfter = require(listFilePath);
+    expect(listAfter.some((li) => li.includes('(done)'))).toBe(true);
+  });
+});
